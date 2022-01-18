065a90db7a21dca0d23281f488f1a21d88336318
junios <junios@naver.com>
2022-01-18 13:55:07 +0900

    accessor 추가 const 추가 접근지정자(private, public) access specifiers 설정

--- Player.h
+++ Player.h
@@ -1,10 +1,27 @@
 #pragma once
+//encapsulenation
 class Player
 {
-public:
+private:
 	int HP;
+public:
 	int MP;
 	int Gold;
+
+public:
+	//Á¢±ÙÀÚ, accessor
+	void SetHP(int NewHP)
+	{
+		if (NewHP >= 0)
+		{
+			HP = NewHP;
+		}
+	}
+
+	const int GetHP() const
+	{
+		return HP;
+	}
 
 	void Collect();
 	void Move();
--- main.cpp
+++ main.cpp
@@ -6,6 +6,11 @@ using namespace std;
 int main()
 {
 	Player NewPlayer;
+	
+	NewPlayer.SetHP(-100);
+	int CurrentHP = NewPlayer.GetHP();
+
+	strcpy()
 
 	return 0;
 }
